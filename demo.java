b3ba95b9a65b9a92a33e40231727202f047274bc
Nithin U <106614289+NithinU2802@users.noreply.github.com>
2023-01-13 01:47:51 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class demo
 {
     public static void main(String arg[]){
+    Scanner x=new Scanner(System.in);
     String a=x.nextLine();
     System.out.print("Welcome You in Programming "+a);
  }
