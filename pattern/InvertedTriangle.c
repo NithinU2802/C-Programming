75d7a5f6ed88642500bd55488f878db1cd4d44e7
Nithin U <106614289+NithinU2802@users.noreply.github.com>
2023-01-27 11:34:57 +0530

    Update InvertedTriangle.c

--- pattern/InvertedTriangle.c
+++ pattern/InvertedTriangle.c
@@ -22,8 +22,8 @@ int main()
     scanf("%d",&n);
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
-            if(i+j>=n-1)    printf("* ");
-            else printf("  ");
+            if(i+j>=n-1)    printf("*");
+            else printf(" ");
         }
         printf("\n");
     }
