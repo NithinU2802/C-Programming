866f27739de200082b8058428b6889493fd014bc
Nithin U <106614289+NithinU2802@users.noreply.github.com>
2023-03-14 18:07:55 +0530

    Create Typedef.c

---
+++ Training/Typedef.c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+typedef implementation...
+
+Output:
+3 a 
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+typedef int Nithin;
+typedef char String;
+
+int main()
+{
+    Nithin a=3;
+    String c='a';
+    printf("%d %c",a,c);
+    return 0;
+}
+
