7f84e1e7a55c4d58f5193515e368ad620546c13a
Nithin U <106614289+NithinU2802@users.noreply.github.com>
2023-03-17 09:11:05 +0530

    Create RegisterFunction.c

---
+++ RegisterFunction.c
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+Nothing changes in output but can provide faster implementation.....
+
+Output:
+12
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    register int n;
+    n=12;
+    printf("%d",n);
+    return 0;
+}
+
