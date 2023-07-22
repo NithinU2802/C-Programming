1d20026d9057a5a03231249dd3ec6a7df8564c2b
Nithin U <106614289+NithinU2802@users.noreply.github.com>
2023-07-22 23:12:52 +0530

    Create CountDigit.c

---
+++ CountDigit.c
@@ -0,0 +1,15 @@
+/**************************************************
+
+    program to implement the digit count using Access 
+Specifier.
+
+**************************************************/
+
+#include <stdio.h>
+
+int main() {
+    int n,c;
+    scanf("%d%n",&n,&c);
+    printf("Count: %d",c);
+    return 0;
+}
